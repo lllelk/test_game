1c54670413a6bde5b69c3bc04d0a5a5219225698
lllelk <neznanov-04@mail.ru>
2024-07-20 17:08:06 +0300

    s

---
+++ src/view/a.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nezna on 20.07.2024.
+//
+
+#include "a.h"
---
+++ src/view/a.h
@@ -0,0 +1,16 @@
+//
+// Created by nezna on 20.07.2024.
+//
+
+#ifndef A_H
+#define A_H
+
+
+
+class a {
+
+};
+
+
+
+#endif //A_H
