722a1fdac2ccc3da165b0a481bcf5f4ddc22514c
Bladforceone <bladforceone@gmail.com>
2024-07-27 12:02:14 +0700

    Delete comments

--- src/view/Window.cpp
+++ src/view/Window.cpp
@@ -1,13 +1,12 @@
 #include "Window.h"
 
 // Инициализация статического члена класса
-Window* Window::window_ = nullptr;
+Window *Window::window_ = nullptr;
 
-// Приватный конструктор
 Window::Window() : sf::RenderWindow(sf::VideoMode(800, 600), "Singleton Window") {}
 
 // Статический метод для получения экземпляра Window
-Window* Window::getInstance() {
+Window *Window::getInstance() {
     if (window_ == nullptr) {
         window_ = new Window();
     }
